5179c87f58d0804a037f478ed45a28d6cb7264e0
박연후 <qkrdusgn00@naver.com>
2024-07-17 11:06:36 +0900

    init: backend 폴더 생성 backend Spring Boot 프로젝트 생성

---
+++ backend/sayif/src/main/java/com/ssafy/sayif/SayifApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.sayif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayifApplication.class, args);
+	}
+
+}
---
+++ backend/sayif/src/test/java/com/ssafy/sayif/SayifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sayif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
