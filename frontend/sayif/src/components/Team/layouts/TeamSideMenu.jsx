aa590920b31781b79bb33e7836e1b7fb709b854f
배선영 <tjsdudqo1234@naver.com>
2024-07-23 11:20:28 +0900

    feat: 팀 사이드 메뉴 생성 버튼 클릭 시 사이드 메뉴 출력 각 메뉴 아이콘 추가

---
+++ frontend/sayif/src/components/Team/layouts/TeamSideMenu.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import QuizIcon from '@mui/icons-material/Quiz';
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
+import '../../../styles/fonts.css'
+import { ListItemIcon, Typography } from '@mui/material';
+
+const icon = {
+    "홈" : <HomeIcon/>,
+    "우리 팀 보기" : <PersonIcon/>,
+    "멘토링 회의" : <VideocamIcon/>,
+    "멘토링 자료" : <AttachFileIcon/>,
+    "Q&A 게시판" : <ContactSupportIcon/>,
+    "메시지" : <QuestionAnswerIcon/>,
+    "퀴즈" : <QuizIcon/>,
+    "사연함" : <NoteAltIcon/>
+}
+
+function TeamSideMenu({offSideMenu}) {
+    const handleShowPage = (select) => {
+        console.log(select);
+    }
+
+    const SideMenuView = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={offSideMenu}>
+        <div style={{textAlign: "center", margin: "30px"}}>
+            <img 
+                src="logo-green.png"
+                width="70px"
+            />
+        </div>
+        <List>
+            {['홈', '우리 팀 보기', '멘토링 회의', '멘토링 자료', 'Q&A 게시판', '메시지', '퀴즈', '사연함'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+                <ListItemButton onClick={() => {handleShowPage(text)}}>
+                    <ListItemIcon sx={{marginLeft: "10px"}}>{icon[text]}</ListItemIcon>
+                    <ListItemText primary={<Typography sx={{fontFamily:"ChosunGu"}}>{text}</Typography>} />
+                </ListItemButton>
+            </ListItem>
+            ))}
+        </List>
+        </Box>
+    );
+
+    return SideMenuView;
+}
+
+export default TeamSideMenu;
