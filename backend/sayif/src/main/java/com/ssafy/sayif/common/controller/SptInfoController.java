622f1dc53af2d57ebb1e927a93565496f300019a
박연후 <qkrdusgn00@naver.com>
2024-07-24 12:13:00 +0900

    refactor: 페이징 기능 추가

--- backend/sayif/src/main/java/com/ssafy/sayif/common/controller/SptInfoController.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/common/controller/SptInfoController.java
@@ -2,12 +2,13 @@ package com.ssafy.sayif.common.controller;
 
 import com.ssafy.sayif.common.entity.SptInfo;
 import com.ssafy.sayif.common.service.SptInfoService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,17 +19,16 @@ public class SptInfoController {
 
     private final SptInfoService sptInfoService;
 
-    @GetMapping
-    public List<SptInfo> getAllSptInfo() {
-        log.info("자립 지원 사업 전체 조회");
-        return sptInfoService.findAll();
-    }
-
     @GetMapping("/{id}")
     public SptInfo getSptInfo(@PathVariable("id") int id) {
-        log.info("자립 지원 사업 단일 조회");
+        log.info("자립 지원 사업 단일 조회 - ID: {}", id);
         return sptInfoService.findById(id).orElse(null);
     }
 
-
+    @GetMapping
+    public Page<SptInfo> getPagedSptInfo(@RequestParam("page") int page,
+        @RequestParam("size") int size) {
+        log.info("페이징 처리 - 페이지: {}, 크기: {}", page, size);
+        return sptInfoService.findAllPaged(page, size);
+    }
 }
--- backend/sayif/src/main/java/com/ssafy/sayif/config/SecurityConfig.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/config/SecurityConfig.java
@@ -8,11 +8,11 @@ import com.ssafy.sayif.member.repository.RefreshRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,7 +34,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception {
         return configuration.getAuthenticationManager();
     }
 
@@ -43,36 +44,37 @@ public class SecurityConfig {
 
         //csrf disable
         http
-                .csrf((auth) -> auth.disable());
+            .csrf(AbstractHttpConfigurer::disable);
 
         //From 로그인 방식 disable
         http
-                .formLogin((auth) -> auth.disable());
+            .formLogin(AbstractHttpConfigurer::disable);
 
         //http basic 인증 방식 disable
         http
-                .httpBasic((auth) -> auth.disable());
-
-
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/member/register").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/reissue").permitAll()
-                        .anyRequest().authenticated());
+            .httpBasic(AbstractHttpConfigurer::disable);
 
         http
-                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
-        http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
-        http
-                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
+            .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/login", "/**", "/member/register").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/reissue").permitAll()
+                .anyRequest().authenticated());
 
+        http
+            .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+        http
+            .addFilterAt(
+                new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil,
+                    refreshRepository), UsernamePasswordAuthenticationFilter.class);
+        http
+            .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository),
+                LogoutFilter.class);
 
         //세션 설정
         http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
