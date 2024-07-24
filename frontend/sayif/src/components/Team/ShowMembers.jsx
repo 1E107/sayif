0751317f3d89de4cf460c877eac195de00fc63ce
배선영 <tjsdudqo1234@naver.com>
2024-07-24 12:19:50 +0900

    feat: Q&A 게시판 리스트 출력 화면력 게시판 리스트 화면 구현

--- frontend/sayif/src/components/Team/ShowMembers.jsx
+++ frontend/sayif/src/components/Team/ShowMembers.jsx
@@ -1,5 +1,4 @@
 import S from './style/ShowMemberStyled';
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
