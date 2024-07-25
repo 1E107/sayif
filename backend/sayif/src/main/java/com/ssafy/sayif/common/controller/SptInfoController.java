af94d6eb5c837390ae2cd768b8278e6fb07d8253
박연후 <qkrdusgn00@naver.com>
2024-07-25 09:22:16 +0900

    refactor: 지원 사업 정보 URL 변경

--- backend/sayif/src/main/java/com/ssafy/sayif/common/controller/SptInfoController.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/common/controller/SptInfoController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,15 +18,15 @@ public class SptInfoController {
 
     private final SptInfoService sptInfoService;
 
-    @GetMapping("/{id}")
+    @GetMapping("/detail/{id}")
     public SptInfo getSptInfo(@PathVariable("id") int id) {
         log.info("자립 지원 사업 단일 조회 - ID: {}", id);
         return sptInfoService.findById(id).orElse(null);
     }
 
-    @GetMapping
-    public Page<SptInfo> getPagedSptInfo(@RequestParam("page") int page,
-        @RequestParam("size") int size) {
+    @GetMapping("/{page}/{size}")
+    public Page<SptInfo> getPagedSptInfo(@PathVariable("page") int page,
+        @PathVariable("size") int size) {
         log.info("페이징 처리 - 페이지: {}, 크기: {}", page, size);
         return sptInfoService.findAllPaged(page, size);
     }
