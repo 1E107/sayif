0391688083b31c0149e59c830d550e67450a70bf
박연후 <qkrdusgn00@naver.com>
2024-07-26 10:14:35 +0900

    feat: 댓글 작성, 수정 구현

---
+++ backend/sayif/src/main/java/com/ssafy/sayif/board/dto/CommentRequestDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.sayif.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class CommentRequestDto {
+
+    private String comment;
+    private String memberId;
+    private int boardId;
+
+}
---
+++ backend/sayif/src/main/java/com/ssafy/sayif/board/service/CommentService.java
@@ -0,0 +1,41 @@
+package com.ssafy.sayif.board.service;
+
+import com.ssafy.sayif.board.converter.CommentConverter;
+import com.ssafy.sayif.board.dto.CommentRequestDto;
+import com.ssafy.sayif.board.entity.Comment;
+import com.ssafy.sayif.board.repository.CommentRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentConverter commentConverter;
+
+    public void writeComment(CommentRequestDto dto) {
+        commentRepository.save(commentConverter.convertToEntity(dto));
+    }
+
+
+    public void modifyComment(int commentId, String memberId, CommentRequestDto dto) {
+        Comment comment =
+            commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid comment ID"));
+
+        if (!memberId.equals(comment.getMember().getMemberId())) {
+            throw new IllegalArgumentException("Invalid member ID");
+        }
+
+        commentRepository.save(
+            Comment.builder()
+                .board(comment.getBoard())
+                .member(comment.getMember())
+                .content(dto.getComment()).build());
+    }
+}
