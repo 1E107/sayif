ee9f0c11d7dadc181c3b58976c44d43501eb6d5e
배선영 <tjsdudqo1234@naver.com>
2024-07-26 15:24:15 +0900

    feat: 채점 결과 화면 출력 채점하기 버튼 클릭 시 정답인지 틀렸는지 알려주는 모달 창 출력

--- frontend/sayif/src/components/Team/StudyQuiz/QuizDetail.jsx
+++ frontend/sayif/src/components/Team/StudyQuiz/QuizDetail.jsx
@@ -6,8 +6,46 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import '../../../styles/fonts.css';
+import { useState } from 'react';
+import Modal from '@mui/material/Modal';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 
 function QuizDetail() {
+    const [isSelect, SetIsSelect] = useState(false);
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => {
+        // 정답인 경우에만 새로고침
+        //window.location.reload();
+        setOpen(false);
+    };
+
+    const handleSelect = index => {
+        SetIsSelect(index);
+    };
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #116530',
+        boxShadow: 24,
+        p: 4,
+        textAlign: 'center',
+    };
+
+    const items = [
+        '1. CPU는 데이터를 영구적으로 저장하고, RAM은 데이터를 처리합니다.',
+        '2. 데이터베이스는 정보를 저장하는 시스템입니다.',
+        '3. 네트워크는 컴퓨터 간의 연결을 제공합니다.',
+        '4. 프로그래밍은 문제 해결을 위한 과정입니다.',
+    ];
+
     const QuizDetailView = (
         <div>
             <S.title>퀴즈 문제</S.title>
@@ -25,112 +63,69 @@ function QuizDetail() {
                 >
                     <nav aria-label="secondary mailbox folders">
                         <List sx={{ textAlign: 'center' }}>
-                            {/* 이거 반복문 돌린다 */}
-                            <ListItem
-                                disablePadding
-                                style={{ marginBottom: '10px' }}
-                            >
-                                <ListItemButton
-                                    style={{
-                                        fontFamily: 'ChosunGu',
-                                        color: 'gray',
-                                    }}
+                            {items.map((item, index) => (
+                                <ListItem
+                                    key={index}
+                                    disablePadding
+                                    style={{ marginBottom: '10px' }}
                                 >
-                                    <ListItemText
-                                        primary={
-                                            <Typography
-                                                style={{
-                                                    fontFamily: 'ChosunGu',
-                                                }}
-                                            >
-                                                1. CPU는 데이터를 영구적으로
-                                                저장하고, RAM은 데이터를
-                                                처리합니다.
-                                            </Typography>
-                                        }
-                                    />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem
-                                disablePadding
-                                style={{ marginBottom: '10px' }}
-                            >
-                                <ListItemButton
-                                    style={{
-                                        fontFamily: 'ChosunGu',
-                                        color: 'gray',
-                                    }}
-                                >
-                                    <ListItemText
-                                        primary={
-                                            <Typography
-                                                style={{
-                                                    fontFamily: 'ChosunGu',
-                                                }}
-                                            >
-                                                1. CPU는 데이터를 영구적으로
-                                                저장하고, RAM은 데이터를
-                                                처리합니다.
-                                            </Typography>
-                                        }
-                                    />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem
-                                disablePadding
-                                style={{ marginBottom: '10px' }}
-                            >
-                                <ListItemButton
-                                    style={{
-                                        fontFamily: 'ChosunGu',
-                                        color: 'gray',
-                                    }}
-                                >
-                                    <ListItemText
-                                        primary={
-                                            <Typography
-                                                style={{
-                                                    fontFamily: 'ChosunGu',
-                                                }}
-                                            >
-                                                1. CPU는 데이터를 영구적으로
-                                                저장하고, RAM은 데이터를
-                                                처리합니다.
-                                            </Typography>
-                                        }
-                                    />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem
-                                disablePadding
-                                style={{ marginBottom: '10px' }}
-                            >
-                                <ListItemButton
-                                    style={{
-                                        fontFamily: 'ChosunGu',
-                                        color: 'gray',
-                                    }}
-                                >
-                                    <ListItemText
-                                        primary={
-                                            <Typography
-                                                style={{
-                                                    fontFamily: 'ChosunGu',
-                                                }}
-                                            >
-                                                1. CPU는 데이터를 영구적으로
-                                                저장하고, RAM은 데이터를
-                                                처리합니다.
-                                            </Typography>
-                                        }
-                                    />
-                                </ListItemButton>
-                            </ListItem>
+                                    <ListItemButton
+                                        style={{
+                                            fontFamily: 'ChosunGu',
+                                            color:
+                                                isSelect === index
+                                                    ? '#116530'
+                                                    : 'gray',
+                                        }}
+                                        onClick={() => handleSelect(index)}
+                                    >
+                                        <ListItemText
+                                            primary={
+                                                <Typography
+                                                    style={{
+                                                        fontFamily: 'ChosunGu',
+                                                    }}
+                                                >
+                                                    {item}
+                                                </Typography>
+                                            }
+                                        />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
                         </List>
                     </nav>
                 </Box>
-                <S.CustomButton variant="contained">채점하기</S.CustomButton>
+                <S.CustomButton variant="contained" onClick={handleOpen}>
+                    채점하기
+                </S.CustomButton>
             </S.Container>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <CheckIcon
+                        style={{ fontSize: '150px', color: '#116530' }}
+                    />
+                    <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                        style={{ fontFamily: 'ChosunGu', color: '#116530' }}
+                    >
+                        정답입니다.
+                    </Typography>
+                </Box>
+                {/* <Box sx={style} style={{ border: '2px solid #BE2E22'}}>
+                    <ClearIcon style={{fontSize: "150px", color: "#BE2E22"}}/>
+                    <Typography id="modal-modal-title" variant="h6" component="h2" style={{fontFamily: "ChosunGu", color: "#BE2E22"}}>
+                        틀렸습니다.
+                    </Typography>
+                </Box> */}
+            </Modal>
         </div>
     );
 
