b520ce812140619367c3119bea8904c69b9b238f
배선영 <tjsdudqo1234@naver.com>
2024-07-27 19:46:41 +0900

    feat: 사연함 상세 화면 구현 포스트잇 클릭 시 상세 내용 출력

--- frontend/sayif/src/components/Team/style/StoryStyled.jsx
+++ frontend/sayif/src/components/Team/style/StoryStyled.jsx
@@ -29,6 +29,28 @@ const PostItWrapper = styled.div`
     text-align: center;
 `
 
+const Modal = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+`
+
+const ModalContent = styled.div`
+    margin-top: 100px;
+    max-width: 500PX;
+    max-height: 500PX;
+    img {
+        width: 100%;
+        height: auto;
+    }
+`
+
 const CustomButton = styled(Button)({
     marginBottom: "50px !important",
     color: "#116530CC !important",
@@ -40,12 +62,24 @@ const CustomButton = styled(Button)({
     marginRight: "10px !important",
 })
 
+const ModalText = styled.div`
+    position: absolute;
+    top: 40%;
+    margin-left: 55px;
+    width: 400px;
+    font-family: ChosunGu;
+    line-height: 1.5em;
+`
+
 const S = {
     Container,
     CustomImg,
     PostItWrapper,
     CustomButton,
     Main,
+    Modal,
+    ModalContent,
+    ModalText
 }
 
 export default S;
