c471a77b55fe586d990aef79974be96aa47d4e65
배선영 <tjsdudqo1234@naver.com>
2024-07-27 23:15:46 +0900

    feat: 코딩 컨벤션 적용

--- frontend/sayif/src/App.js
+++ frontend/sayif/src/App.js
@@ -1,47 +1,47 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import Header from "./components/layouts/Header";
-import IntroPage from "./pages/Introduction/IntroPage";
-import CommunityPage from "./pages/Community/CommunityPage";
-import ApplyPage from "./pages/Mentoring/ApplyPage";
-import TeamPage from "./pages/Team/TeamPage";
-import TeamHeader from "./components/Team/layouts/TeamHeader";
-import BoardPage from "./pages/Team/BoardPage";
-import LoginPage from "./pages/Member/LoginPage";
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import Header from './components/layouts/Header';
+import IntroPage from './pages/Introduction/IntroPage';
+import CommunityPage from './pages/Community/CommunityPage';
+import ApplyPage from './pages/Mentoring/ApplyPage';
+import TeamPage from './pages/Team/TeamPage';
+import TeamHeader from './components/Team/layouts/TeamHeader';
+import BoardPage from './pages/Team/BoardPage';
+import LoginPage from './pages/Member/LoginPage';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <>
-      {location.pathname.startsWith("/team") ? (
-        <TeamHeader></TeamHeader>
-      ) : (
-        <Header></Header>
-      )}
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/serviceIntroduction" element={<IntroPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/apply-mentoring" element={<ApplyPage />} />
-        <Route path="/team/*" element={<TeamPage />} />
-        <Route path="/team/board/detail/:id" element = {<BoardPage/>} /> 
-        <Route path="/member">
-          <Route path="login" element= {<LoginPage/>}></Route>
-        </Route>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            {location.pathname.startsWith('/team') ? (
+                <TeamHeader></TeamHeader>
+            ) : (
+                <Header></Header>
+            )}
+            <Routes>
+                <Route path="/" element={<MainPage />}></Route>
+                <Route path="/serviceIntroduction" element={<IntroPage />} />
+                <Route path="/community" element={<CommunityPage />} />
+                <Route path="/apply-mentoring" element={<ApplyPage />} />
+                <Route path="/team/*" element={<TeamPage />} />
+                <Route path="/team/board/detail/:id" element={<BoardPage />} />
+                <Route path="/member">
+                    <Route path="login" element={<LoginPage />}></Route>
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 const AppMain = () => {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
 };
 
 export default AppMain;
