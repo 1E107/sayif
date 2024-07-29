d94baa750c0baa4af502a2811b9ebc0d1ec9103b
이창호 <ckdgh6589@naver.com>
2024-07-29 14:02:17 +0900

    test: front의 설정 및 cors 반영 SecurityConfig와 CorsMvcConfig package-lock.json package.json TeamMainStyled,ShowMemberStyled수정

---
+++ frontend/sayif/src/components/Team/style/TeamMainStyled.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import '../../../styles/fonts.css'
+
+const Container = styled.div`
+   
+`;
+
+const ImageBox = styled.div`
+    height: 400px;
+    width: 400px;
+    background-color: white;
+    border-radius: 30px;
+`;
+
+const Wrapper = styled.div`
+`;
+
+const TeamNameText = styled.div`
+    color: #116530;
+    font-size: 25px;
+    font-family: ChosunGu;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 20px;
+`
+
+const TeamScoreText = styled.div`
+    font-size: 20px;
+    margin-top: 5px;
+    color: #116530;
+`
+
+const ScoreContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const S = {
+    Container,
+    ImageBox,
+    TeamNameText,
+    Wrapper,
+    TeamScoreText,
+    ScoreContainer
+}
+
+export default S;
