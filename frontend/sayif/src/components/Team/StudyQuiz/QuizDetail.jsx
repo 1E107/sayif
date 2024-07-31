341b253f94d6c19f0ca0afb105c7a5d0d1482616
배선영 <tjsdudqo1234@naver.com>
2024-07-31 12:17:30 +0900

    feat: 퀴즈 리스트 출력 및 상세 출력 api 요청 리스트 출력 후 상세 퀴즈 내용 출력 완료 유저가 선택한 문항이 정답인지 결과 화면 출력 완료

--- frontend/sayif/src/components/Team/StudyQuiz/QuizDetail.jsx
+++ frontend/sayif/src/components/Team/StudyQuiz/QuizDetail.jsx
@@ -11,18 +11,23 @@ import Modal from '@mui/material/Modal';
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
 
-function QuizDetail() {
-    const [isSelect, SetIsSelect] = useState("");
+function QuizDetail({ quiz }) {
+    const [isSelect, SetIsSelect] = useState('');
+    const [open, SetOpen] = useState(false);
+    const [isCorrect, SetIsCorrect] = useState(false);
+    const [openCorrect, SetOpenCorrect] = useState(false);
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
+    const handleOpen = () => {
+        if (isCorrect) SetOpenCorrect(true);
+        SetOpen(true);
+    };
     const handleClose = () => {
-        // 정답인 경우에만 새로고침
-        //window.location.reload();
-        setOpen(false);
+        SetOpen(false);
+        if (isCorrect) window.location.reload();
     };
 
-    const handleSelect = index => {
+    const handleSelect = (index, correct) => {
+        SetIsCorrect(correct);
         SetIsSelect(index);
     };
 
@@ -39,22 +44,11 @@ function QuizDetail() {
         textAlign: 'center',
     };
 
-    const items = [
-        '1. CPU는 데이터를 영구적으로 저장하고, RAM은 데이터를 처리합니다.',
-        '2. 데이터베이스는 정보를 저장하는 시스템입니다.',
-        '3. 네트워크는 컴퓨터 간의 연결을 제공합니다.',
-        '4. 프로그래밍은 문제 해결을 위한 과정입니다.',
-    ];
-
     const QuizDetailView = (
         <div>
             <S.title>퀴즈 문제</S.title>
             <S.Container>
-                <S.ChapterTitle>IT 기초 개념 소개</S.ChapterTitle>
-                <S.TestTitle>
-                    컴퓨터의 중앙 처리 장치(CPU)와 주 기억 장치(RAM)의 역할에
-                    대한 설명 중 올바른 것은 무엇입니까?
-                </S.TestTitle>
+                <S.TestTitle>{quiz.question}</S.TestTitle>
                 <Box
                     sx={{
                         bgcolor: 'background.paper',
@@ -63,7 +57,7 @@ function QuizDetail() {
                 >
                     <nav aria-label="secondary mailbox folders">
                         <List sx={{ textAlign: 'center' }}>
-                            {items.map((item, index) => (
+                            {quiz.quiz_choice_dto.map((item, index) => (
                                 <ListItem
                                     key={index}
                                     disablePadding
@@ -77,7 +71,9 @@ function QuizDetail() {
                                                     ? '#116530'
                                                     : 'gray',
                                         }}
-                                        onClick={() => handleSelect(index)}
+                                        onClick={() =>
+                                            handleSelect(index, item.is_answer)
+                                        }
                                     >
                                         <ListItemText
                                             primary={
@@ -86,7 +82,7 @@ function QuizDetail() {
                                                         fontFamily: 'ChosunGu',
                                                     }}
                                                 >
-                                                    {item}
+                                                    {item.content}
                                                 </Typography>
                                             }
                                         />
@@ -106,25 +102,35 @@ function QuizDetail() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
-                    <CheckIcon
-                        style={{ fontSize: '150px', color: '#116530' }}
-                    />
-                    <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                        style={{ fontFamily: 'ChosunGu', color: '#116530' }}
-                    >
-                        정답입니다.
-                    </Typography>
-                </Box>
-                {/* <Box sx={style} style={{ border: '2px solid #BE2E22'}}>
-                    <ClearIcon style={{fontSize: "150px", color: "#BE2E22"}}/>
-                    <Typography id="modal-modal-title" variant="h6" component="h2" style={{fontFamily: "ChosunGu", color: "#BE2E22"}}>
-                        틀렸습니다.
-                    </Typography>
-                </Box> */}
+                {openCorrect ? (
+                    <Box sx={style}>
+                        <CheckIcon
+                            style={{ fontSize: '150px', color: '#116530' }}
+                        />
+                        <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                            style={{ fontFamily: 'ChosunGu', color: '#116530' }}
+                        >
+                            정답입니다.
+                        </Typography>
+                    </Box>
+                ) : (
+                    <Box sx={style} style={{ border: '2px solid #BE2E22' }}>
+                        <ClearIcon
+                            style={{ fontSize: '150px', color: '#BE2E22' }}
+                        />
+                        <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                            style={{ fontFamily: 'ChosunGu', color: '#BE2E22' }}
+                        >
+                            틀렸습니다.
+                        </Typography>
+                    </Box>
+                )}
             </Modal>
         </div>
     );
