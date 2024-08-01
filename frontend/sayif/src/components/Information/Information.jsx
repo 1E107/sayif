2fbd7b96d320159da29c01806c6c85bbe14d0742
배선영 <tjsdudqo1234@naver.com>
2024-08-02 02:04:21 +0900

    feat: 자립 정보 목록 화면 구현 자립 정보 리스트 화면 구현 각 카드 컴포넌트 클릭 시 상세 페이지로 이동

---
+++ frontend/sayif/src/components/Information/Information.jsx
@@ -0,0 +1,103 @@
+import S from './InformationStyled'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { useNavigate } from 'react-router-dom';
+
+function Information() {
+    const navigate = useNavigate();
+
+    const handleShowDetail = (id) => {
+        navigate(`/support-information/${id}`)
+    }
+
+    const InformationView = (
+        <S.Container>
+            <S.Title>자립 지원 정보</S.Title>
+            <div>
+                <S.Line/>
+            </div>
+            <div style={{display: "flex", marginBottom: "30px"}}>
+                <Card sx={{ maxWidth: 300, borderRadius: 7}} style={{marginLeft: "30px"}} onClick={() => {handleShowDetail(1)}}>
+                    <CardMedia
+                        sx={{ height: 200 }}
+                        image="/img/info-temp-img.jpg"
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <S.InfoTitle gutterBottom variant="h5" component="div">
+                        자립준비청년 자조모임 지원 사업
+                        </S.InfoTitle>
+                        <S.InfoContent variant="body2" color="text.secondary">2024.04.19 ~ 2024.05.31</S.InfoContent>
+                        <S.InfoContent variant="body2" color="text.secondary">경상남도 자립지원전담기관</S.InfoContent>
+                    </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 300, borderRadius: 7 }} style={{marginLeft: "30px"}}>
+                    <CardMedia
+                        sx={{ height: 200 }}
+                        image="/img/info-temp-img.jpg"
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <S.InfoTitle gutterBottom variant="h5" component="div">
+                        자립준비청년 자조모임 지원 사업
+                        </S.InfoTitle>
+                        <S.InfoContent variant="body2" color="text.secondary">2024.04.19 ~ 2024.05.31</S.InfoContent>
+                        <S.InfoContent variant="body2" color="text.secondary">경상남도 자립지원전담기관</S.InfoContent>
+                    </CardContent>
+                </Card>
+            </div>
+            <div style={{display: "flex", marginBottom: "30px"}}>
+                <Card sx={{ maxWidth: 300, borderRadius: 7}} style={{marginLeft: "30px"}}>
+                    <CardMedia
+                        sx={{ height: 200 }}
+                        image="/img/info-temp-img.jpg"
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <S.InfoTitle gutterBottom variant="h5" component="div">
+                        자립준비청년 자조모임 지원 사업
+                        </S.InfoTitle>
+                        <S.InfoContent variant="body2" color="text.secondary">2024.04.19 ~ 2024.05.31</S.InfoContent>
+                        <S.InfoContent variant="body2" color="text.secondary">경상남도 자립지원전담기관</S.InfoContent>
+                    </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 300, borderRadius: 7 }} style={{marginLeft: "30px"}}>
+                    <CardMedia
+                        sx={{ height: 200 }}
+                        image="/img/info-temp-img.jpg"
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <S.InfoTitle gutterBottom variant="h5" component="div">
+                        자립준비청년 자조모임 지원 사업
+                        </S.InfoTitle>
+                        <S.InfoContent variant="body2" color="text.secondary">2024.04.19 ~ 2024.05.31</S.InfoContent>
+                        <S.InfoContent variant="body2" color="text.secondary">경상남도 자립지원전담기관</S.InfoContent>
+                    </CardContent>
+                </Card>
+            </div>
+            <Stack spacing={2} style={{margin:"50px 0px 50px 0px"}}>
+                <Pagination count={10} variant="outlined" sx={{
+                    '& .MuiPaginationItem-root': {
+                        bgcolor: 'white',
+                        color: '#116530', 
+                        '&.Mui-selected': {
+                            bgcolor: '#116530', 
+                            color: 'white',
+                        },
+                    },
+                }}/>
+            </Stack>
+        </S.Container>
+    )
+
+    return InformationView;
+}
+
+export default Information;
---
+++ frontend/sayif/src/components/Information/InformationStyled.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import Typography from '@mui/material/Typography';
+import '../../styles/fonts.css'
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`
+const Title = styled.div`
+    color: #116530;
+    font-family: ONE-Mobile-POP;
+    font-size: 30px;
+    margin-top: 130px;
+`
+
+const Line = styled.hr`
+    margin-top: 15px;
+    width: 210px;
+    border-top: 2px solid #116530;
+    margin-bottom: 80px;
+`
+
+const InfoTitle = styled(Typography)({
+    fontFamily: "ChosunGu !important",
+    fontSize: "18px !important",
+    padding: "10px !important",
+});
+
+const InfoContent = styled(Typography)({
+    fontFamily: "ChosunGu !important",
+    padding: "1px",
+})
+
+const S = {
+    Container,
+    Title,
+    Line,
+    InfoTitle,
+    InfoContent
+}
+
+export default S; 
