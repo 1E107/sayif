f38206849742e49838a917563e22ee50db17e319
이승지 <esg0107@gmail.com>
2024-08-01 16:43:02 +0900

    refactor: 웹 소켓 진행중

--- backend/sayif/src/main/java/com/ssafy/sayif/config/WebSocketConfig.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/config/WebSocketConfig.java
@@ -1,19 +1,30 @@
 package com.ssafy.sayif.config;
 
+import ch.qos.logback.classic.pattern.MessageConverter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.ssafy.sayif.member.jwt.JWTUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JWTUtil jwtUtil;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-            .setAllowedOrigins("http://localhost:8080/")
+            .setAllowedOrigins("http://localhost:9090/", "http://localhost:3000/")
             .withSockJS();
     }
 
@@ -21,6 +32,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new FilterChannelInterceptor(jwtUtil));
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<org.springframework.messaging.converter.MessageConverter> messageConverters) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        converter.setObjectMapper(objectMapper);
+        messageConverters.add(converter);
+        return false;
     }
 }
 
