83f4bc6d96747c11f7fe3856a948d08e8eb0e668
박연후 <qkrdusgn00@naver.com>
2024-08-02 11:14:13 +0900

    comment: BoardController 주석 추가

--- backend/sayif/src/main/java/com/ssafy/sayif/board/controller/BoardController.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/board/controller/BoardController.java
@@ -3,13 +3,14 @@ package com.ssafy.sayif.board.controller;
 import com.ssafy.sayif.board.dto.PostRequestDto;
 import com.ssafy.sayif.board.entity.BoardType;
 import com.ssafy.sayif.board.service.BoardService;
-import com.ssafy.sayif.common.exception.ImageStorageException;
+import com.ssafy.sayif.common.exception.FileStorageException;
 import com.ssafy.sayif.common.service.FileService;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,60 +33,81 @@ public class BoardController {
     private final String bucketName = "board-images";
 
     /**
-     * 게시물을 작성합니다. 게시물과 파일을 함께 업로드할 수 있습니다.
+     * 게시물을 작성합니다. 파일이 업로드된 경우, 해당 파일을 함께 저장합니다.
      *
      * @param dto  게시물 작성 요청 데이터
-     * @param file 업로드할 이미지 파일
+     * @param file 업로드할 이미지 파일 (선택적)
      * @return 생성된 게시물에 대한 응답
      */
     @PostMapping
     public ResponseEntity<?> writePost(@RequestPart("post") PostRequestDto dto,
-        @RequestPart("file") MultipartFile file) {
+        @RequestPart(value = "file", required = false) MultipartFile file) {
         try {
-            saveFileAndSetFilename(dto, file);
+            // 파일이 존재하는 경우에만 파일 저장 및 파일 이름 설정
+            if (file != null && !file.isEmpty()) {
+                saveFileAndSetFilename(dto, file);
+            }
 
-            // 게시물 수정 서비스 호출
-            ResponseEntity.ok(boardService.writePost(dto));
-            return ResponseEntity.ok("Post created successfully with image");
+            // 게시물 작성 서비스 호출
+            boardService.writePost(dto);
+            return ResponseEntity.ok("Post created successfully with file");
         } catch (IOException e) {
-            return ResponseEntity.ok("Failed to create post: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed to create post: " + e.getMessage());
         }
     }
 
     /**
-     * 게시물을 수정합니다.
+     * 게시물을 수정합니다. 파일이 업로드된 경우, 해당 파일을 함께 저장합니다.
      *
      * @param id   수정할 게시물의 ID
      * @param dto  게시물 수정 요청 데이터
-     * @param file 업로드할 이미지 파일
+     * @param file 업로드할 이미지 파일 (선택적)
      * @return 수정된 게시물에 대한 응답
      */
     @PutMapping("/{id}")
     public ResponseEntity<?> modifyPost(@PathVariable("id") int id,
         @RequestPart("post") PostRequestDto dto,
-        @RequestPart("file") MultipartFile file) {
+        @RequestPart(value = "file", required = false) MultipartFile file) {
         try {
-            saveFileAndSetFilename(dto, file);
+            // 파일이 존재하는 경우에만 파일 저장 및 파일 이름 설정
+            if (file != null && !file.isEmpty()) {
+                saveFileAndSetFilename(dto, file);
+            }
 
-            // 게시물 작성 서비스 호출
-            ResponseEntity.ok(boardService.modifyPost(id, dto));
-            return ResponseEntity.ok("Post created successfully with image");
+            // 게시물 수정 서비스 호출
+            boardService.modifyPost(id, dto);
+            return ResponseEntity.ok("Post modified successfully with file");
         } catch (IOException e) {
-            return ResponseEntity.ok("Failed to create post: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed to modify post: " + e.getMessage());
         }
     }
 
+
+    /**
+     * 파일을 Minio 서버에 저장하고, 저장된 파일의 이름을 게시물 DTO에 설정합니다.
+     *
+     * @param dto  게시물에 대한 요청 데이터를 담고 있는 DTO 객체
+     * @param file 저장할 파일을 담고 있는 MultipartFile 객체
+     * @throws IOException 파일 처리 중 오류가 발생한 경우
+     */
     private void saveFileAndSetFilename(PostRequestDto dto, MultipartFile file) throws IOException {
+        // MultipartFile 객체에서 파일의 바이트 배열을 가져옵니다.
         byte[] fileContent = file.getBytes();
+
+        // Minio 서버에 파일을 저장하고, 저장된 파일의 이름을 반환받습니다.
         String filename = fileService.saveFileToMinio(fileContent, bucketName);
 
+        // 파일이 제대로 저장되지 않았거나, 반환된 파일 이름이 null인 경우 예외를 발생시킵니다.
         if (filename == null) {
-            throw new ImageStorageException("Failed to save file.");
+            throw new FileStorageException("Failed to save file.");
         }
 
-        // 이미지 파일 이름을 게시물 DTO에 설정
+        // 저장된 파일의 이름을 게시물 DTO에 설정합니다.
         dto.setFile(filename);
     }
+
 
     /**
      * 게시물을 삭제합니다.
