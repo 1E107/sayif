cc771ed2e377cb44a855589e2731afe154483bd1
이창호 <ckdgh6589@naver.com>
2024-08-02 12:33:44 +0900

    feat: / -> /api로 변경

--- backend/sayif/src/main/java/com/ssafy/sayif/domain/test/controller/TestController.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/domain/test/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class TestController {
-    @GetMapping("/")
+    @GetMapping("/api")
     public String dockerTest() {
         return "hello docker!";
     }
