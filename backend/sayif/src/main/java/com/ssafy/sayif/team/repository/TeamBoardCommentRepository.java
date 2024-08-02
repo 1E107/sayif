41f8b39e959c6d8f29981d9946a7e3747bd71bbc
박연후 <qkrdusgn00@naver.com>
2024-08-02 12:32:25 +0900

    refactor: QnA 게시판 댓글 서비스 수정

--- backend/sayif/src/main/java/com/ssafy/sayif/team/repository/TeamBoardCommentRepository.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/team/repository/TeamBoardCommentRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.sayif.team.repository;
 
 import com.ssafy.sayif.team.entity.QnaAnswer;
+import com.ssafy.sayif.team.entity.TeamBoard;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamBoardCommentRepository extends JpaRepository<QnaAnswer, Integer> {
+
+    List<QnaAnswer> getAllByTeamBoard(TeamBoard teamBoard);
 
 }
