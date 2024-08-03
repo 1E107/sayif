4535f3a007c010e2d7d4d0aac91f441eb795bec6
이승지 <esg0107@pusan.ac.kr>
2024-08-03 18:33:53 +0900

    refactor: SockJS 초기화와 웹소켓 연결 프로토콜 변경

--- frontend/sayif/src/api/WebSocketService.js
+++ frontend/sayif/src/api/WebSocketService.js
@@ -17,7 +17,7 @@ class WebSocketService {
     this.token = token;
 
     this.client = new Client({
-      brokerURL: 'ws://i11e107.p.ssafy.io:7777/ws',
+      brokerURL: 'wss://i11e107.p.ssafy.io:7777/ws',
       connectHeaders: {
         Authorization: `Bearer ${token}`
       },
@@ -27,7 +27,7 @@ class WebSocketService {
       reconnectDelay: 5000,
       heartbeatIncoming: 4000,
       heartbeatOutgoing: 4000,
-      webSocketFactory: () => new SockJS(`http://i11e107.p.ssafy.io:7777/ws`)
+      webSocketFactory: () => new SockJS(`https://i11e107.p.ssafy.io:7777/ws`)
     });
 
     this.client.onConnect = (frame) => {
