d306bffb29a41cb6dbcda9b590dd808c93f8a690
이창호 <ckdgh6589@naver.com>
2024-08-04 15:01:55 +0900

    test: corsmvcconfig https://i11e107.p.ssafy.io 추가

--- backend/sayif/src/main/java/com/ssafy/sayif/config/CorsMvcConfig.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/config/CorsMvcConfig.java
@@ -10,11 +10,11 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+        corsRegistry.addMappaing("/**")
+                .allowedOrigins("http://localhost:3000","https://i11e107.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") //클라이언트 측의 CORS 요청에 허용되는 헤더 지정
                 .exposedHeaders("access") // 클라이언트가 응답에 접근할 수 있는 헤더 지정
-                .allowCredentials(true);
+                .allowCredentials(true);s
     }
 }
