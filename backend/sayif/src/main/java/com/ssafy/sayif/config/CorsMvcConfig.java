5a78d7b331f772ef5189e04c63cd035355de02f4
이창호 <ckdgh6589@naver.com>
2024-08-04 15:03:26 +0900

    test : 오타수정

--- backend/sayif/src/main/java/com/ssafy/sayif/config/CorsMvcConfig.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/config/CorsMvcConfig.java
@@ -15,6 +15,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*") //클라이언트 측의 CORS 요청에 허용되는 헤더 지정
                 .exposedHeaders("access") // 클라이언트가 응답에 접근할 수 있는 헤더 지정
-                .allowCredentials(true);s
+                .allowCredentials(true);
     }
 }
