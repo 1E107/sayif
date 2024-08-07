9596f995f3157939b6fc95e326bb15072467d5ac
박연후 <qkrdusgn00@naver.com>
2024-08-07 11:05:29 +0900

    fix: 멘토링 그룹 생성 멘토링 그룹 생성 안되던 버그 수정

--- backend/sayif/src/main/java/com/ssafy/sayif/team/dto/MentoringRecruitRequest.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/team/dto/MentoringRecruitRequest.java
@@ -1,10 +1,14 @@
 package com.ssafy.sayif.team.dto;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 @Data
 public class MentoringRecruitRequest {
+
+    @JsonProperty("start_date")
     private String startDate;
+    @JsonProperty("day_of_week")
     private String dayOfWeek;
     private String time;
     private String pmam;
