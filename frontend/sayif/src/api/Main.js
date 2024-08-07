8611a2596250b1c35b4b89add1d955ba298b797a
박연후 <qkrdusgn00@naver.com>
2024-08-08 00:22:07 +0900

    rename: 댓글 api 요청 필드명 수정

--- frontend/sayif/src/api/Main.js
+++ frontend/sayif/src/api/Main.js
@@ -1,74 +1,85 @@
 // 소통 공간, 정보 공간 페이지에서 필요한 api가 정리되어 있습니다.
 
-import axios from "axios";
-import { API_BASE_URL } from "./config";
+import axios from 'axios';
+import { API_BASE_URL } from './config';
 
 export const getSupportInfo = async (page, size, token) => {
-    try{
-        const response = await axios.get(`${API_BASE_URL}/spt-info/${page}/${size}`, {
-            headers: {Authorization: `Bearer ${token}`}
-        });
-        return response;
-    }catch(error) {
-        throw error;
-    }
-}
-
-export const GetDetailSupportInfo = async(id, token) => {
-    try{
-        const response  = await axios.get(`${API_BASE_URL}/spt-info/detail/${id}`, {
-            headers: {Authorization: `Bearer ${token}`}
-        });
-        return response;
-    }catch(error) {
-        throw error;
-    }
-}
-
-export const GetCommunityList = async(token, type, page, size) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/board/${type}/${page}/${size}`, {
-            headers: {Authorization: `Bearer ${token}`}
-        });
+        const response = await axios.get(
+            `${API_BASE_URL}/spt-info/${page}/${size}`,
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            },
+        );
         return response;
-    }
-    catch(error) {
+    } catch (error) {
         throw error;
     }
-}
+};
 
-export const GetCommunityDetail = async(token, id) => {
+export const GetDetailSupportInfo = async (id, token) => {
+    try {
+        const response = await axios.get(
+            `${API_BASE_URL}/spt-info/detail/${id}`,
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            },
+        );
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const GetCommunityList = async (token, type, page, size) => {
+    try {
+        const response = await axios.get(
+            `${API_BASE_URL}/board/${type}/${page}/${size}`,
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            },
+        );
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const GetCommunityDetail = async (token, id) => {
     try {
         const response = await axios.get(`${API_BASE_URL}/board/detail/${id}`, {
-            headers: {Authorization: `Bearer ${token}`}
+            headers: { Authorization: `Bearer ${token}` },
         });
         return response;
-    }catch(error) {
+    } catch (error) {
         throw error;
     }
-}
+};
 
-export const GetCommunityComment = async(token, id) => {
+export const GetCommunityComment = async (token, id) => {
     try {
         const response = await axios.get(`${API_BASE_URL}/comment/${id}`, {
-            headers: {Authorization: `Bearer ${token}`}
+            headers: { Authorization: `Bearer ${token}` },
         });
         return response;
-    }catch(error) {
+    } catch (error) {
         throw error;
     }
-}
+};
 
-export const PostCommunityComment = async(token, id, comment) => {
-    const data = {comment: comment};
+export const PostCommunityComment = async (token, id, comment) => {
+    const data = { content: comment };
 
     try {
-        const response = await axios.post(`${API_BASE_URL}/comment/${id}`, data, {
-            headers: {Authorization: `Bearer ${token}`}
-        });
+        const response = await axios.post(
+            `${API_BASE_URL}/comment/${id}`,
+            data,
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            },
+        );
         return response;
-    }
-    catch(error) {
+    } catch (error) {
         throw error;
     }
-}
+};
