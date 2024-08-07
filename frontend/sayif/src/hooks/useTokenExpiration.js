929c17d70e89ecc9033cc017025c0d693fbad5ce
배선영 <tjsdudqo1234@naver.com>
2024-08-08 00:28:18 +0900

    feat: 푼 퀴즈 상태 처리 풀어서 정답 맞춘 퀴즈는 다시 못 풀게 막기 보기 선택 잘 보이게 css 수정

--- frontend/sayif/src/hooks/useTokenExpiration.js
+++ frontend/sayif/src/hooks/useTokenExpiration.js
@@ -1,45 +1,62 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setExpirationdate, setMember, setToken } from "../redux/modules/member";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    setExpirationdate,
+    setMember,
+    setToken,
+} from '../redux/modules/member';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getNewToken } from '../api/MemberApi';
 
 const useTokenExpiration = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const {expirationDate} = useSelector(state => state.member);
+    const { expirationDate } = useSelector(state => state.member);
     const [isLoggedOut, setIsLoggedOut] = useState(false);
     const location = useLocation();
 
     const logout = () => {
-        if(!isLoggedOut) {
+        if (!isLoggedOut) {
             localStorage.removeItem('persist:root');
             dispatch(setToken(null));
             dispatch(setMember({}));
             dispatch(setExpirationdate(null));
             setIsLoggedOut(true);
-            alert("로그아웃 되었습니다. 다시 로그인하세요!");
+            alert('로그아웃 되었습니다. 다시 로그인하세요!');
             navigate('/member/login');
         }
-    }
+    };
 
     useEffect(() => {
         const expirationDateUtc = new Date(expirationDate);
         const currentDate = new Date();
-    
+
         const checkTokenExpiration = () => {
             if (expirationDateUtc <= currentDate) {
                 logout();
+                //토큰 재발급
+                const callNewToken = async () => {
+                    try {
+                        await getNewToken();
+                        // const response = await getNewToken();
+                        // console.log(response);
+                    } catch (error) {
+                        console.log(error);
+                    }
+                };
+
+                callNewToken();
             }
         };
 
         checkTokenExpiration();
-        
+
         const interval = setInterval(() => {
             checkTokenExpiration();
         }, 60000);
 
         return () => clearInterval(interval);
     }, [dispatch, location.pathname]);
-}
+};
 
 export default useTokenExpiration;
