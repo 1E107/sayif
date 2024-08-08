4dbe5b0689714a61b3b9d4822a1a21ddc7edabe8
배선영 <tjsdudqo1234@naver.com>
2024-08-08 10:29:58 +0900

    feat: 회원가입 부분 수정사항 전체 수정 프로필 등록 다른 페이지로 생성 빈칸 있으면 페이지 이동 못하게 제한 -> 공백으로 회원 정보 등록 못하도록 프로필 이미지 선택 안하면 기본 이미지로 등록

---
+++ frontend/sayif/src/components/Member/style/RegistProfileImgStyled.jsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import '../../../styles/fonts.css';
+
+const Container = styled.div`
+    height: 300px;
+    width: 500px;
+    margin: 250px 0px 100px 50px;
+    border-radius: 20px;
+    margin-right: 60px;
+    box-shadow:
+        0 3px 6px rgba(0, 0, 0, 0.16),
+        0 3px 6px rgba(0, 0, 0, 0.23);
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 50px 0px 50px 0px;
+`;
+
+const TitleText = styled.div`
+    color: #116530;
+    font-family: ChosunGu;
+    font-weight: bold;
+    font-size: 17px;
+`;
+
+const ImgSelect = styled.img`
+    width: 200px;
+    border-radius: 200px;
+    border: 1px solid #e9ecef;
+    margin-top: 20px;
+    cursor: pointer;
+`;
+
+const SubmitBtn = styled(Button)({
+    marginTop: '30px !important',
+    fontFamily: 'ChosunGu !important',
+    backgroundColor: '#116530 !important',
+});
+
+const ExplanText = styled.div`
+    font-family: ChosunGu;
+    font-size: 14px;
+    margin-top: 20px;
+`;
+
+const S = {
+    Container,
+    TitleText,
+    ImgSelect,
+    SubmitBtn,
+    ExplanText,
+};
+
+export default S;
--- frontend/sayif/src/pages/Member/RegisterPage.jsx
+++ frontend/sayif/src/pages/Member/RegisterPage.jsx
@@ -1,11 +1,12 @@
-import styled from "styled-components";
-import Register from "../../components/Member/Register";
-import ProofDocuments from "../../components/Member/ProofDocuments";
+import styled from 'styled-components';
+import Register from '../../components/Member/Register';
+import ProofDocuments from '../../components/Member/ProofDocuments';
+import RegistProfileImg from '../../components/Member/RegistProfileImg';
 import '../../styles/fonts.css';
-import { useLocation } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 const Main = styled.div`
     height: 700px;
-`
+`;
 
 const Item = styled.div`
     height: 650px;
@@ -13,22 +14,22 @@ const Item = styled.div`
     background-color: #116530;
     border-radius: 0px 0px 0px 1000px;
     display: flex;
-    align-items : center;
+    align-items: center;
     justify-content: flex-end;
-`
+`;
 
 const ItemText = styled.div`
-    color: #E8E8CC;
+    color: #e8e8cc;
     font-size: 65px;
     font-family: Freesentation-9Black;
     letter-spacing: 5px;
     margin-right: 50px;
-`
+`;
 
 const ItemGroup = styled.div`
     display: flex;
     justify-content: space-between;
-`
+`;
 
 const RegisterPage = () => {
     const location = useLocation();
@@ -36,12 +37,19 @@ const RegisterPage = () => {
     return (
         <Main>
             <ItemGroup>
-                {location.pathname === '/member/regist' && <Register/>}
-                {location.pathname === '/member/regist/proof-documents' && <ProofDocuments/>}
-                <Item><ItemText>함께 해요</ItemText></Item>
+                {location.pathname === '/member/regist' && <Register />}
+                {location.pathname === '/member/regist/proof-documents' && (
+                    <ProofDocuments />
+                )}
+                {location.pathname === '/member/regist/profile-img' && (
+                    <RegistProfileImg />
+                )}
+                <Item>
+                    <ItemText>함께 해요</ItemText>
+                </Item>
             </ItemGroup>
         </Main>
-    )
-}
+    );
+};
 
 export default RegisterPage;
