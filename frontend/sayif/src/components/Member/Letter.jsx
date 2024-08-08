99ee8b6d61368d340930e6c00779165f461eb976
배선영 <tjsdudqo1234@naver.com>
2024-08-09 01:59:11 +0900

    feat: 쪽지 상세 조회 모달 구현 리스트 클릭 시 해당하는 상세 내용 모달로 출력

--- frontend/sayif/src/components/Member/Letter.jsx
+++ frontend/sayif/src/components/Member/Letter.jsx
@@ -4,21 +4,24 @@ import Stack from '@mui/material/Stack';
 import { useEffect, useState } from 'react';
 import { getList } from '../../api/Letter';
 import { useSelector } from 'react-redux';
+import LetterModal from './LetterModal';
 
 function Letter() {
     const { token, member } = useSelector(state => state.member);
     const [totalData, SetTotalData] = useState(0);
     const [itemList, SetItemList] = useState([]);
     const itemsPerPage = 6;
+    const [page, SetPage] = useState(1);
+    const [showModal, SetShowModal] = useState(false);
+    const [selectId, SetSelectId] = useState();
 
     useEffect(() => {
         const callGetList = async () => {
             try {
-                const response = await getList(token, 1, 6);
+                const response = await getList(token, page, 6);
                 if (response.status === 200) {
                     SetTotalData(response.data.totalElements);
                     SetItemList(response.data.content);
-                    console.log(response.data);
                 }
             } catch (error) {
                 console.log(error);
@@ -28,29 +31,50 @@ function Letter() {
         callGetList();
     }, []);
 
-    const LetterView = (
-        <S.Container>
-            <S.HeaderText>쪽지함</S.HeaderText>
-            <S.ListWrapper>
-                {itemList.map(data => {
-                    return (
-                        <S.ListItem>
-                            <S.ListText>{data.sendId}</S.ListText>
-                            <S.ListText>{data.title}</S.ListText>
-                            <S.ListText>{data.createdAt}</S.ListText>
-                        </S.ListItem>
-                    );
-                })}
+    const handlePageChange = (event, value) => {
+        SetPage(value);
+    };
 
-                <S.PageWrapper>
-                    <Stack spacing={2}>
-                        <Pagination
-                            count={Math.ceil(totalData / itemsPerPage)}
-                        />
-                    </Stack>
-                </S.PageWrapper>
-            </S.ListWrapper>
-        </S.Container>
+    const handleModal = () => {
+        SetShowModal(!showModal);
+    };
+
+    const handleDetailPage = id => {
+        SetSelectId(id);
+        handleModal();
+    };
+
+    const LetterView = (
+        <>
+            <S.Container>
+                <S.HeaderText>쪽지함</S.HeaderText>
+                <S.ListWrapper>
+                    {itemList.map(data => {
+                        return (
+                            <S.ListItem
+                                onClick={() => {
+                                    handleDetailPage(data.id);
+                                }}
+                            >
+                                <S.ListText>{data.sendId}</S.ListText>
+                                <S.ListText>{data.title}</S.ListText>
+                                <S.ListText>{data.createdAt}</S.ListText>
+                            </S.ListItem>
+                        );
+                    })}
+
+                    <S.PageWrapper>
+                        <Stack spacing={2}>
+                            <Pagination
+                                onChange={handlePageChange}
+                                count={Math.ceil(totalData / itemsPerPage)}
+                            />
+                        </Stack>
+                    </S.PageWrapper>
+                </S.ListWrapper>
+            </S.Container>
+            {showModal && <LetterModal onClose={handleModal} id={selectId} />}
+        </>
     );
     return LetterView;
 }
