9dfca212876eda1365c3cb53a3690ead432415c9
방혁 <qkdgur0323@gmail.com>
2024-08-09 15:33:47 +0900

    feature: 백엔드 팀 정보 API 수정

--- backend/sayif/src/main/java/com/ssafy/sayif/team/service/TeamService.java
+++ backend/sayif/src/main/java/com/ssafy/sayif/team/service/TeamService.java
@@ -39,7 +39,8 @@ public class TeamService {
     private final TeamMsgRepository teamMsgRepository;
 
     public List<Member> getMembersByTeamId(Integer teamId) {
-        return memberRepository.findByTeamId(teamId);
+        List<Member> members = memberRepository.findByTeamId(teamId);
+        return members;
     }
 
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 팀 상태 변경
