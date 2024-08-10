d4c2946a03c893e8359717a498d6e5249415e837
이창호 <ckdgh6589@naver.com>
2024-08-10 20:19:00 +0900

    feat: 서비스 소개 페이지 만듦

--- frontend/sayif/src/components/Introduction/Introduction.jsx
+++ frontend/sayif/src/components/Introduction/Introduction.jsx
@@ -1,9 +1,33 @@
-function Introduction() {
-    const IntroductionView = (
-        <div>Introduction Page</div>
-    )
+import S from "./styled";
+import ApplyPage from "../../pages/Mentoring/ApplyPage";
 
-    return IntroductionView;
+function Introduction() {
+    const Main_URL = 'http://localhost:3000/'
+    return (
+        <>
+        <S.Container>
+            <S.Title>서비스 소개</S.Title>
+        </S.Container>
+        <S.ContentContainer>
+            <S.TextContainer>
+                <S.SubTitle>자립 준비 청년을 위한
+                     SSAFY 교육생들의 IT 멘토링 서비스</S.SubTitle>
+                <S.Description>
+                    WebRTC 기반 교육, 챗봇 활용 복습, 커뮤니티 지원 및 IT 멘토링을 통해 자립 준비 청년의 
+                    지식 향상과 정보 제공 서비스
+                </S.Description>
+                <S.LinkContainer>
+                    <S.Link href="https://www.ssafy.com/">SSAFY 홈페이지 바로가기</S.Link>
+                    <S.Link href={`${Main_URL}/apply-mentoring/`}>멘토링 신청 바로가기</S.Link>
+                    <S.Link href={`${Main_URL}/create-mentoring/`}>멘토링 그룹 생성 바로가기</S.Link>
+                </S.LinkContainer>
+            </S.TextContainer>
+            <S.ImageContainer>
+                <S.Image src={`${process.env.PUBLIC_URL}/service_intro.png`} alt="서비스 소개" />
+            </S.ImageContainer>
+        </S.ContentContainer>
+        </>
+    );
 }
 
 export default Introduction;
