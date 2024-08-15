ac57e288c04dd731fca715d2d4924b030a2f214e
이승지 <esg0107@pusan.ac.kr>
2024-08-15 15:55:22 +0900

    fix: pageRefreshOnReturn 코드 주석 처리

--- frontend/sayif/src/components/Team/PageRefreshOnReturn.jsx
+++ frontend/sayif/src/components/Team/PageRefreshOnReturn.jsx
@@ -1,32 +1,32 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
 
-const PageRefreshOnReturn = () => {
-    const location = useLocation();
+// const PageRefreshOnReturn = () => {
+//     const location = useLocation();
 
-    useEffect(() => {
-        const currentPath = location.pathname;
-        const hash = location.hash;
+//     useEffect(() => {
+//         const currentPath = location.pathname;
+//         const hash = location.hash;
 
-        // 저장된 리프레시 상태 확인
-        const refreshKey = 'needsRefresh';
-        const needsRefresh = localStorage.getItem(refreshKey);
+//         // 저장된 리프레시 상태 확인
+//         const refreshKey = 'needsRefresh';
+//         const needsRefresh = localStorage.getItem(refreshKey);
 
-        // 새로고침이 필요한 경우
-        if (currentPath === '/team/message' && !hash) {
-            if (needsRefresh) {
-                localStorage.removeItem(refreshKey); // 새로고침 후 키 제거
-                window.location.reload(); // 페이지 새로 고침
-            } else {
-                localStorage.setItem(refreshKey, 'true'); // 새로고침 필요 상태 저장
-            }
-        } else {
-            // URL이 변경되면 키 제거
-            localStorage.removeItem(refreshKey);
-        }
-    }, [location]);
+//         // 새로고침이 필요한 경우
+//         if (currentPath === '/team/message' && !hash) {
+//             if (needsRefresh) {
+//                 localStorage.removeItem(refreshKey); // 새로고침 후 키 제거
+//                 window.location.reload(); // 페이지 새로 고침
+//             } else {
+//                 localStorage.setItem(refreshKey, 'true'); // 새로고침 필요 상태 저장
+//             }
+//         } else {
+//             // URL이 변경되면 키 제거
+//             localStorage.removeItem(refreshKey);
+//         }
+//     }, [location]);
 
-    return null;
-};
+//     return null;
+// };
 
-export default PageRefreshOnReturn;
+// export default PageRefreshOnReturn;
